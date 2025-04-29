827ee920e6a3175e8165a3d8ded51d5dbccff982
ThiagoSantos7 <seu-email>
2025-04-29 14:26:45 -0300

    funções

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const area = document.getElementById("area");
 
 const entrar = () => {
-  const nome = prompt("Digite seu nome");
+  const nome = prompt("Digite seu nome: ");
 
   if (nome === "" || nome === null) {
-    alert("Ops, algo deu errado...");
+    alert("Ops! Algo deu errado...");
     area.innerHTML = "Clique no botão e tente novamente...";
   } else {
     area.innerHTML = "Bem vindo " + nome + " ";
@@ -17,7 +17,22 @@ const entrar = () => {
   }
 };
 
-function sair() {
+const sair = () => {
   alert("Até mais!");
-  area.innerHTML = "Voce saiu...";
+  area.innerHTML = "Você saiu...";
+};
+
+function mediaAluno(nota1, nota2) {
+  const media = (nota1 + nota2) / 2;
+
+  if (media >= 7) {
+    console.log("Aluno aprovado com média: " + media);
+  } else if (media < 7) {
+    console.log("Aluno reprovado com média: " + media);
+  }
+}
+
+function aluno(nome, curso) {
+  const menssagem = "Bem vindo " + nome + " ao curso de " + curso;
+  console.log(menssagem);
 }
